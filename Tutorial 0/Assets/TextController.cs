c86e775a6d5fdbf27d4222cf7823c968b6c606e5
xArtoriasx <60054783+xArtoriasx@users.noreply.github.com>
2020-01-19 01:20:41 -0500

    tutorial 0

---
+++ Tutorial 0/Assets/TextController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextController : MonoBehaviour
+{
+    public Text newText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+     if (Input.GetKeyDown("space"))
+{
+newText.text = "Hello, my name is Alec Nicholson!";
+}
+     if (Input.GetKey("escape"))
+{
+Application.Quit();
+}   
+    }
+}
